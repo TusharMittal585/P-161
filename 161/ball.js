51dc6c47392f51ecac8822f7818056f12ff8436d
TusharMittal585 <77103048+TusharMittal585@users.noreply.github.com>
2023-04-24 16:38:04 +0530

    Add files via upload

---
+++ 161/ball.js
@@ -0,0 +1,45 @@
+AFRAME.registerComponent("balls", {
+    init: function () {
+      this.shootBall();
+    },
+    shootBall: function () {
+      window.addEventListener("keydown", (e) => {
+        if (e.key === 32) {
+          var ball = document.createElement("a-entity");
+  
+          ball.setAttribute("geometry", {
+            primitive: "sphere",
+            radius: 0.7,
+          });
+  
+          ball.setAttribute("material", "color", "black");
+  
+          var cam = document.querySelector("#camera");
+  
+          pos = cam.getAttribute("position");
+  
+          ball.setAttribute("position", {
+            x: pos.x,
+            y: pos.y,
+            z: pos.z,
+          });
+  
+          var camera = document.querySelector("#camera").object3D;
+  
+          //get the camera direction as Three.js Vector
+          var direction = new THREE.Vector3();
+          camera.getWorldDirection(direction);
+  
+          //set the velocity and it's direction
+          ball.setAttribute("velocity", direction.multiplyScalar(-10));
+  
+          var scene = document.querySelector("#scene");
+  
+          scene.appendChild(ball);
+        }
+      });
+    },
+  });
+  
+  
+  
